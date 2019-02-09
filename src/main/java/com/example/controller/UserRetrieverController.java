0aba97befeb55898cec25226f4f9a1cbc358bc12
Fatima <tayyba.fatima@teradata.com>
2019-02-09 23:09:04 +0500

    Signed-off-by: Fatima <tayyba.fatima@teradata.com> -Logger to Write into file

--- src/main/java/com/example/controller/UserRetrieverController.java
+++ src/main/java/com/example/controller/UserRetrieverController.java
@@ -12,11 +12,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @RestController
 
 public class UserRetrieverController {
     private final AppProperties appProperties;
-	 
+    private static final Logger logger = LogManager.getLogger(UserRetrieverController.class);
+
 	 @Autowired
 	 public UserRetrieverController(AppProperties appProperties) {
 		 this.appProperties=appProperties;
@@ -24,6 +28,7 @@ public class UserRetrieverController {
 	 @RequestMapping(method = RequestMethod.GET, value="/user/alluser")
 	 @ResponseBody
 	  public List<User> getAllUsers() {
+	    logger.info("-----Info log----- Request Method : GET, value:/user/alluser " );
 		System.out.println("Application Properties"+ this.appProperties.getFormat());
 	  return UserRegistration.getInstance().getUserRecords();
 	  }
